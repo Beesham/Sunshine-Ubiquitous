331e9cca5ccdfe9cffc803f5f8786aa9157b043b
Beesham Sarendranauth <sarendranauth.beesham@gmail.com>
2017-01-01 20:15:41 -0500

    add high,low paint

--- sunshinewearable/src/main/java/com/beesham/sunshinewearable/DigitialSunshineWatchFace.java
+++ sunshinewearable/src/main/java/com/beesham/sunshinewearable/DigitialSunshineWatchFace.java
@@ -172,6 +172,12 @@ public class DigitialSunshineWatchFace extends CanvasWatchFaceService {
             mDividerPaint = new Paint();
             mDividerPaint = createDividerPaint();
 
+            mHighPaint = new Paint();
+            mHighPaint = createTextPaint(resources.getColor(R.color.light_grey));
+
+            mLowPaint = new Paint();
+            mLowPaint = createTextPaint(resources.getColor(R.color.light_grey));
+
         }
 
         @Override
@@ -344,6 +350,8 @@ public class DigitialSunshineWatchFace extends CanvasWatchFaceService {
 
             canvas.drawLine(bounds.centerX(), bounds.centerY(), bounds.centerX() + 20f, bounds.centerY(), mDividerPaint);
 
+            canvas.drawText("25", bounds.centerX(), bounds.centerY() + 20f, mHighPaint);
+            canvas.drawText("10", bounds.centerX()+20f, bounds.centerY() + 20f, mLowPaint);
         }
 
         /**
